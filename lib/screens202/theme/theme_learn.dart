bae6b2f3a3b993ea1513f6ffc0dafabd8316da2f
Taha Can Şenel <147408850+Korugan32@users.noreply.github.com>
2025-06-30 18:03:50 +0300

    Theme Learn

---
+++ lib/screens202/theme/theme_learn.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ThemeLearn {
+  ThemeData themeLearn = ThemeData(
+    cardColor: Colors.amber
+    // Theme'lar burdan ayarlanip uygulamada kullanilabilir.
+  );
+}
