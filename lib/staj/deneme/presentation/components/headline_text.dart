290ddc0998752a4a5324f713544a744c76d2dfd7
Taha Can Şenel <147408850+Korugan32@users.noreply.github.com>
2025-07-25 13:22:41 +0300

    Add HeadlineText component for displaying bold headlines

---
+++ lib/staj/deneme/presentation/components/headline_text.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class HeadlineText extends StatelessWidget {
+  const HeadlineText({super.key, required this.text});
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(text, style: Theme.of(context).textTheme.headlineLarge?.copyWith(fontWeight: FontWeight.bold));
+  }
+}
