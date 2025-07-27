a75786d52ba586d15050e387e3bb50acd10e3da3
Taha Can Şenel <147408850+Korugan32@users.noreply.github.com>
2025-07-27 15:21:33 +0300

    Add localization support for English and Turkish languages

---
+++ lib/l10n/app_localizations_en.dart
@@ -0,0 +1,43 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get account => 'Account';
+
+  @override
+  String get country => 'Country';
+
+  @override
+  String get data_usage => 'Data Usage';
+
+  @override
+  String get display_and_sound => 'Display and Sound';
+
+  @override
+  String get english => 'English';
+
+  @override
+  String get general_settings => 'General Settings';
+
+  @override
+  String get notifactions => 'Notifications';
+
+  @override
+  String get permissions => 'Permissions';
+
+  @override
+  String get phone_language => 'Phone Language';
+
+  @override
+  String get privacy => 'Privacy';
+
+  @override
+  String get turkish => 'Turkish';
+}
---
+++ lib/l10n/app_localizations_tr.dart
@@ -0,0 +1,43 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Turkish (`tr`).
+class AppLocalizationsTr extends AppLocalizations {
+  AppLocalizationsTr([String locale = 'tr']) : super(locale);
+
+  @override
+  String get account => 'Hesap';
+
+  @override
+  String get country => 'Ülke';
+
+  @override
+  String get data_usage => 'Veri Kullanımı';
+
+  @override
+  String get display_and_sound => 'Görüntü ve Ses';
+
+  @override
+  String get english => 'İngilizce';
+
+  @override
+  String get general_settings => 'Genel Ayarlar';
+
+  @override
+  String get notifactions => 'Bildirimler';
+
+  @override
+  String get permissions => 'İzinler';
+
+  @override
+  String get phone_language => 'Telefon Dili';
+
+  @override
+  String get privacy => 'Gizlilik';
+
+  @override
+  String get turkish => 'Türkçe';
+}
